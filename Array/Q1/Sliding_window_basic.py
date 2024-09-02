ea9c1f4d925c274625e789f0191830ac348be5d6
Harsh Raghuwanshi <harsharaghuwanshi094@gmail.com>
2024-09-02 20:49:32 +0530

    Array/Q1/Sliding window Basic

---
+++ Array/Q1/Sliding_window_basic.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr = list(map(int, input().split()))
+k = int(input())
+#an empty list to store the negative integers
+result = []
+# loop through array to check each window
+for i in range(n - k + 1):
+    window = arr[i:i+k]
+    # Finding the first negative number in window
+    found_negative = False
+    for num in window:
+        if num < 0:
+            result.append(str(num))
+            found_negative = True
+            break
+     # IF all numbers are positive    
+    if not found_negative:
+        result.append("0")
+        
+#print all result
+print(" ".join(result))
