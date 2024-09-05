88bf908206ff3d1b7795a6b14f7ad4abdf50fab6
Harsh Raghuwanshi <harsharaghuwanshi094@gmail.com>
2024-09-06 00:18:31 +0530

    Air Encapsulated between the right and Left wall of an array

--- Array/Q11/Air_encapsulated_betweenwalls.py
+++ Array/Q11/Air_encapsulated_betweenwalls.py
@@ -15,3 +15,23 @@ def find_max_height(arr):
         right_max[i] = max(right_max[i+1], arr[i])
 
     return left_max, right_max
+
+def calculate_air_encapsulated(arr):
+    n = len(arr)
+    #get the left max and right max height
+    left_max, right_max = find_max_height(arr)
+    #initialize the air to 
+    total_air = 0
+    #calculate air encapsulated at each position
+    for i in range(n):
+        air_at_i = min(left_max[i], right_max[i]) - arr[i]
+        if air_at_i > 0:
+            total_air += air_at_i
+
+    return total_air
+
+#input
+n = int(input())
+arr = list(map(int, input().split()))
+#print result
+print(calculate_air_encapsulated(arr))
