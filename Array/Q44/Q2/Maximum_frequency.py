e3355e04192fb92cf4d5c66f440dee113d26d399
Harsh Raghuwanshi <harsharaghuwanshi094@gmail.com>
2024-09-17 00:31:08 +0530

    Print Maximum Frequency

--- Array/Q44/Q2/Maximum_frequency.py
+++ Array/Q44/Q2/Maximum_frequency.py
@@ -1,19 +1,23 @@
 def max_frequency(arr):
+    #Initialize an empty dictionary
     freq = {}
     for i in arr:
         if i in freq:
             freq[i] += 1
         else:
             freq[i] = 1
-    
+    #count the Maximum frequency
     max_count = max(freq.values())
+    # array to store maximum frequency element
     maximum_frequency_element = []
     for i, count in freq.items():
         if count == max_count:
             maximum_frequency_element.append(str(i))
-        
+
+    #print the result    
     print(" ".join(maximum_frequency_element))
 
+#Inputs
 n = int(input())
 arr = list(map(int, input().split())) 
 max_frequency(arr)
