1b6cbeb6001a626e397ea4cb9106bcaf197b31fb
Harsh Raghuwanshi <harsharaghuwanshi094@gmail.com>
2024-09-20 15:07:34 +0530

    find if a string 'S2’' can be obtained by rotating another string 'S1’' by 2 places using Stack

--- Array/Q51/RotationByTwoPlace_in_String.py
+++ Array/Q51/RotationByTwoPlace_in_String.py
@@ -2,13 +2,24 @@ def rotation_by_two_place(str1, str2):
     # Check if length of both strings is the same
     if len(str1) != len(str2):
         return 0
+    
+    # Left Rotation using Stack
+    stack_left = list(S1[:2])  # First 2 characters
+    left_rotated = S1[2:] + ''.join(stack_left)  # Remaining + first 2 characters
+
+    # Right Rotation using Stack
+    stack_right = list(S1[-2:])  # Last 2 characters
+    right_rotated = ''.join(stack_right) + S1[:-2]  # Last 2 characters + remaining
+
+            # Above is using stack AND Below is without stack
+
     # rotate left by two place in string 1 
-    left_rotate = str1[2:] + str1[:2]
+    left_rotated = str1[2:] + str1[:2]
     #rotate right by two place in str str1
-    right_rotate = str1[-2:] + str1[:-2]
+    right_rotated = str1[-2:] + str1[:-2]
     
     #Check if either of the rotations matches String 2 
-    if str2 == left_rotate or str == right_rotate:
+    if str2 == left_rotated or str == right_rotated:
         return 1 
     else:
         return 0
