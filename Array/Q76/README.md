9e592722aa999c300d9dd42781ad8078608a58e5
Harsh Raghuwanshi <harsharaghuwanshi094@gmail.com>
2024-10-02 19:08:00 +0530

    Convert a Number from Decimal to Binary

---
+++ Array/Q76/README.md
@@ -0,0 +1,2 @@
+# Given a number N in decimal convert it into binary value.
+Input Size : N <= 100000
---
+++ Array/Q76/decimalTo_binary.py
@@ -0,0 +1,12 @@
+num = int(input())
+binary_representation = bin(num)[2:] # Removing '0b' prefix
+print(binary_representation)
+
+'''
+Sample Testcase :
+INPUT
+5
+OUTPUT
+101
+
+'''
