089f2055e35617cb69d9be564163a5114d06a413
Harsh Raghuwanshi <harsharaghuwanshi094@gmail.com>
2024-10-03 01:58:08 +0530

    Right shift the Array by K

---
+++ Array/Q77/Readme.md
@@ -0,0 +1,2 @@
+# Given two numbers N,K followed by an array of N elements, print the array after doing right shift K times (in cyclic manner).
+Input Size : 1 <= N, K <= 100000
---
+++ Array/Q77/Right_shift_byK.py
@@ -0,0 +1,23 @@
+def right_shift_k_times(arr, K, N):
+    # Normalize K to avoid unnecessary rotations
+    K = K % N
+    # Split and concatenate
+    if K == 0:
+        return arr
+    else:
+        return arr[-K:] + arr[:-K]
+    
+N,K = map(int, input().split())
+arr = list(map(int, input().split()))
+res = right_shift_k_times(arr, K, N)
+print(*res)
+
+'''
+Sample Testcase :
+INPUT
+3 2
+7 2 3
+OUTPUT
+2 3 7
+
+'''
